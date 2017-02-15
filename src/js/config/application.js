20d4c558cb585edc1fc2ac280699a963c217c207
Benoît Simard (@logisima) <contact@bsimard.com>
2017-02-15 16:35:40 +0100

    Turn off log by default

--- src/js/config/application.js
+++ src/js/config/application.js
@@ -1,11 +1,11 @@
 /**
  * Configuration object for the application
  */
-const configApplication = { 
+const configApplication = {
     name: "Stefi Graph",
-    logo: './assets/logo.png',	
-    logLevel: 'Debug', // Error, Warning, Info, Debug
-    logPattern: '.*', 
+    logo: './assets/logo.png',
+    logLevel: 'Error', // Error, Warning, Info, Debug
+    logPattern: '.*',
     queryHistorySize: 100,
     baobabHistorySize: 0,
     persistance: 'Off'  // Off, LocalStorage, Url
