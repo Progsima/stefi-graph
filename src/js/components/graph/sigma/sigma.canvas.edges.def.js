47b6ce9040fb33f8d3fc2fe629b14e3b801d1cd9
Benoît Simard (@logisima) <contact@bsimard.com>
2017-02-22 11:47:15 +0100

    Good scale/size for edges

--- src/js/components/graph/sigma/sigma.canvas.edges.def.js
+++ src/js/components/graph/sigma/sigma.canvas.edges.def.js
@@ -13,77 +13,39 @@
    * @param  {configurable}             settings     The settings function.
    */
   sigma.canvas.edges.def = function(edge, source, target, context, settings) {
-    var color = edge.color,
-        prefix = settings('prefix') || '',
-        edgeColor = settings('edgeColor'),
-        defaultNodeColor = settings('defaultNodeColor'),
-        defaultEdgeColor = settings('defaultEdgeColor'),
-        cp = {},
-        size = edge[prefix + 'size'] || 1,
-        count = edge.order || 0,
-        tSize = target[prefix + 'size'],
-        sX = source[prefix + 'x'],
-        sY = source[prefix + 'y'],
-        tX = target[prefix + 'x'],
-        tY = target[prefix + 'y'],
-        aSize = Math.max(size * 2.5, settings('minArrowSize')),
-        d,
-        aX,
-        aY,
-        vX,
-        vY;
+    var prefix = settings('prefix') || '';
 
-    cp = (source.id === target.id) ?
-      sigma.canvas.utils.getSelfLoopControlPoints(sX, sY, tSize, count) :
-      sigma.canvas.utils.getQuadraticControlPoint(sX, sY, tX, tY, count);
+    // Compute variables
+    var edgeColor = edge.color || settings('defaultEdgeColor');
+    var hidden = edge.hidden || false;
+    var active = edge.active || false;
+    var edgeOrder = edge.order || 0;
+    var activeBorderColor = settings('activeBorderColor') || '#FF0000';
+    var activeBorderSizeRatio = settings('activeBorderSizeRatio') || 0.1;
+    var edgeSize = edge[prefix + 'size'] || settings('defaultEdgeSize');
+    var sourceX = source[prefix + 'x'];
+    var sourceY = source[prefix + 'y'];
+    var targetX = target[prefix + 'x'];
+    var targetY = target[prefix + 'y'];
 
-    if (source.id === target.id) {
-      d = Math.sqrt(Math.pow(tX - cp.x1, 2) + Math.pow(tY - cp.y1, 2));
-      aX = cp.x1 + (tX - cp.x1) * (d - aSize - tSize) / d;
-      aY = cp.y1 + (tY - cp.y1) * (d - aSize - tSize) / d;
-      vX = (tX - cp.x1) * aSize / d;
-      vY = (tY - cp.y1) * aSize / d;
-    }
-    else {
-      d = Math.sqrt(Math.pow(tX - cp.x, 2) + Math.pow(tY - cp.y, 2));
-      aX = cp.x + (tX - cp.x) * (d - aSize - tSize) / d;
-      aY = cp.y + (tY - cp.y) * (d - aSize - tSize) / d;
-      vX = (tX - cp.x) * aSize / d;
-      vY = (tY - cp.y) * aSize / d;
-    }
+    if(!hidden) {
 
-    if (!color)
-      switch (edgeColor) {
-        case 'source':
-          color = source.color || defaultNodeColor;
-          break;
-        case 'target':
-          color = target.color || defaultNodeColor;
-          break;
-        default:
-          color = defaultEdgeColor;
-          break;
+      if(active){
+        if(source.id === target.id) {
+          sigma.canvas.utils.drawSelfEdgeArrow(context, sourceX, sourceY, source[prefix + 'size'], activeBorderColor, edgeSize * activeBorderSizeRatio, edgeOrder);
+        }
+        else {
+          sigma.canvas.utils.drawEdgeArrow(context, sourceX, sourceY, targetX, targetY, activeBorderColor, edgeSize * activeBorderSizeRatio, edgeOrder);
+        }
       }
 
-    context.strokeStyle = color;
-    context.lineWidth = size;
-    context.beginPath();
-    context.moveTo(sX, sY);
-    if (source.id === target.id) {
-      context.bezierCurveTo(cp.x2, cp.y2, cp.x1, cp.y1, aX, aY);
-    } else {
-      context.quadraticCurveTo(cp.x, cp.y, aX, aY);
+      if(source.id === target.id) {
+        sigma.canvas.utils.drawSelfEdgeArrow(context, sourceX, sourceY, source[prefix + 'size'], edgeColor, edgeSize, edgeOrder);
+      }
+      else {
+        sigma.canvas.utils.drawEdgeArrow(context, sourceX, sourceY, targetX, targetY, edgeColor, edgeSize, edgeOrder);
+      }
     }
-    context.stroke();
-
-    context.fillStyle = color;
-    context.beginPath();
-    context.moveTo(aX + vX, aY + vY);
-    context.lineTo(aX + vY * 0.6, aY - vX * 0.6);
-    context.lineTo(aX - vY * 0.6, aY + vX * 0.6);
-    context.lineTo(aX + vX, aY + vY);
-    context.closePath();
-    context.fill();
-
   };
+
 })();
--- src/js/components/graph/sigma/sigma.canvas.utils.js
+++ src/js/components/graph/sigma/sigma.canvas.utils.js
@@ -34,6 +34,45 @@
 
   }
 
+  sigma.canvas.utils.drawArrow = function(context, aX, aY, bX, bY, cX, cY, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.moveTo(aX, aY);
+    context.lineTo(bX, bY);
+    context.lineTo(cX, cY);
+    context.lineTo(aX, aY);
+    context.closePath();
+    context.fill();
+  }
+
+  sigma.canvas.utils.drawSelfEdgeArrow = function(context, nodeX, nodeY, nodeSize, color, size, order) {
+    var controlPoint = sigma.canvas.utils.getSelfLoopControlPoints(nodeX, nodeY, nodeSize, size, order);
+
+    context.strokeStyle = color;
+    context.lineWidth = size;
+    context.beginPath();
+    context.moveTo(nodeX, nodeY);
+    context.bezierCurveTo(controlPoint.x2, controlPoint.y2, controlPoint.x1, controlPoint.y1, nodeX, nodeY);
+    context.stroke();
+  }
+
+  sigma.canvas.utils.drawEdgeArrow = function(context, sourceX, sourceY, targetX, targetY, color, size, order) {
+    var controlPoint = sigma.canvas.utils.getQuadraticControlPoint(sourceX, sourceY, targetX, targetY, size, order);
+
+    context.strokeStyle = color;
+    context.lineWidth = size;
+    context.beginPath();
+    context.moveTo(sourceX, sourceY);
+    context.quadraticCurveTo(controlPoint.x, controlPoint.y, targetX, targetY);
+    context.stroke();
+  }
+
+
+  /**
+   * Reset the canvas context with default value.
+   *
+   * @param {object} context The canvas context
+   */
   sigma.canvas.utils.resetContext = function(context) {
     context.shadowOffsetX = 0;
     context.shadowOffsetY = 0;
@@ -45,18 +84,19 @@
   /**
    * Return the control point coordinates for a quadratic bezier curve.
    *
-   * @param  {number} x1  The X coordinate of the start point.
-   * @param  {number} y1  The Y coordinate of the start point.
-   * @param  {number} x2  The X coordinate of the end point.
-   * @param  {number} y2  The Y coordinate of the end point.
-   * @param  {number} a   Modifier for the amplitude of the curve.
-   * @return {x,y}        The control point coordinates.
+   * @param  {number} x1       The X coordinate of the start point.
+   * @param  {number} y1       The Y coordinate of the start point.
+   * @param  {number} x2       The X coordinate of the end point.
+   * @param  {number} y2       The Y coordinate of the end point.
+   * @param  {number} edgeSize The edge size.
+   * @param  {number} order    Modifier for the amplitude of the curve.
+   * @return {x,y}             The control point coordinates.
    */
-  sigma.canvas.utils.getQuadraticControlPoint = function(x1, y1, x2, y2, a) {
-    a = a || 0;
+  sigma.canvas.utils.getQuadraticControlPoint = function(x1, y1, x2, y2, edgeSize, order) {
+    order = order || 0;
     return {
-      x: (x1 + x2) / 2 + (y2 - y1) / (60 / (15 + a)),
-      y: (y1 + y2) / 2 + (x1 - x2) / (60 / (15 + a))
+      x: (x1 + x2) / 2 + (y2 - y1) / (60 / (5 + order * Math.log(edgeSize + 1) * 5)),
+      y: (y1 + y2) / 2 + (x1 - x2) / (60 / (5 + order * Math.log(edgeSize + 1) * 5))
     };
   };
 
@@ -65,19 +105,20 @@
    * where the start point is also the end point) computed as a cubic bezier
    * curve.
    *
-   * @param  {number} x    The X coordinate of the node.
-   * @param  {number} y    The Y coordinate of the node.
-   * @param  {number} size The node size.
-   * @param  {number} a    Modifier to the loop size.
-   * @return {x1,y1,x2,y2} The coordinates of the two control points.
+   * @param  {number} x        The X coordinate of the node.
+   * @param  {number} y        The Y coordinate of the node.
+   * @param  {number} nodeSize The node size.
+   * @param  {number} edgeSize The edge size.
+   * @param  {number} order    Modifier to the loop size.
+   * @return {x1,y1,x2,y2}     The coordinates of the two control points.
    */
-  sigma.canvas.utils.getSelfLoopControlPoints = function(x , y, size, a) {
-    a = a || 0;
+  sigma.canvas.utils.getSelfLoopControlPoints = function(x , y, nodeSize, edgeSize, order) {
+    order = order || 0;
     return {
-      x1: x - (size + a) * 7,
+      x1: x - (nodeSize * (order+1)) * Math.log(edgeSize + 1),
       y1: y,
       x2: x,
-      y2: y + (size + a) * 7
+      y2: y + (nodeSize * (order+1) ) * Math.log(edgeSize + 1)
     };
   };
 
